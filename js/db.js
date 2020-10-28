197243a52d7753cf6ba7f91d508627c69e3b7467
Muhammad Bagas Pradana <54340164+bagas050201@users.noreply.github.com>
2020-10-28 17:57:23 +0700

    Update db.js
    add save data to idexed.db

--- js/db.js
+++ js/db.js
@@ -1,1 +1,21 @@
-p
+let dbPromised = idb.open("bola-spanyol", 1, function(upgradeDb) {
+    let articlesObjectStore = upgradeDb.createObjectStore("detail-info", {
+      keyPath: "id"
+    });
+    articlesObjectStore.createIndex("name", "name", { unique: false });
+  });
+
+  function saveForLater(information) {
+    console.log(information)
+    dbPromised
+      .then(db => {
+        let tx = db.transaction("detail-info", "readwrite");
+        let store = tx.objectStore("detail-info");
+        console.log(information);
+        store.add(information);
+        return tx.complete;
+      })
+      .then(() => {
+        console.log("information berhasil di simpan.");
+      });
+  }
