9ff6022b05b64ae72106820682a5ce206c68d2dc
Muhammad Bagas Pradana <54340164+bagas050201@users.noreply.github.com>
2020-10-29 22:08:31 +0700

    generate push notif from google firebase in (32-64)

--- js/register_sw.js
+++ js/register_sw.js
@@ -1,88 +1,65 @@
-/** 
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
-        .register("/service-worker.js")
-        .then(function() {
-          console.log("Pendaftaran ServiceWorker berhasil");
-        })
-        .catch(function() {
-          console.log("Pendaftaran ServiceWorker gagal");
-        });
-    });
-  } else {
-    console.log("ServiceWorker belum didukung browser ini.");
-  }
+if (!('serviceWorker' in navigator)) {
+  console.log("Service worker tidak didukung browser ini.");
+} else {
+  registerServiceWorker();
+  requestPermission();
 
   document.addEventListener("DOMContentLoaded", function() {
     getAllStandings();
-  });
-
-**/
-
-
-
-  if (!('serviceWorker' in navigator)) {
-    console.log("Service worker tidak didukung browser ini.");
-  } else {
-    registerServiceWorker();
-    requestPermission();
-
-    document.addEventListener("DOMContentLoaded", function() {
-      getAllStandings();
-  });
-  };
-  function registerServiceWorker() {
-    return navigator.serviceWorker.register('service-worker.js')
-      .then(function (registration) {
-        console.log('Registrasi service worker berhasil.');
-        return registration;
-      })
-      .catch(function (err) {
-        console.error('Registrasi service worker gagal.', err);
-      });
-    }
-  function requestPermission() {
+});
+};
+function registerServiceWorker() {
+  return navigator.serviceWorker.register('service-worker.js')
+    .then(function (registration) {
+      console.log('Registrasi service worker berhasil.');
+      return registration;
+    })
+    .catch(function (err) {
+      console.error('Registrasi service worker gagal.', err);
+    });
+  }
+function requestPermission() {
 if ('Notification' in window) {
 Notification.requestPermission().then(function (result) {
-  if (result === "denied") {
-    console.log("Fitur notifikasi tidak diijinkan.");
-    return;
-  } else if (result === "default") {
-    console.error("Pengguna menutup kotak dialog permintaan ijin.");
-    return;
-  }
-  
-  if (('PushManager' in window)) {
-    navigator.serviceWorker.getRegistration().then(function(registration) {
-        registration.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: urlBase64ToUint8Array("BIVS5WUCKOVRZyfTkzPeM2_2fBQiPpO8SaEUeftgSxOQX2sDbhJUKojwrHQbzF2jTg_ylp9OY1BiMvlUNeeRtEo")
-        }).then(function(subscribe) {
-            console.log('Berhasil melakukan subscribe dengan endpoint: ', subscribe.endpoint);
-            console.log('Berhasil melakukan subscribe dengan p256dh key: ', btoa(String.fromCharCode.apply(
-                null, new Uint8Array(subscribe.getKey('p256dh')))));
-            console.log('Berhasil melakukan subscribe dengan auth key: ', btoa(String.fromCharCode.apply(
-                null, new Uint8Array(subscribe.getKey('auth')))));
-        }).catch(function(e) {
-            console.error('Tidak dapat melakukan subscribe ', e.message);
-        });
-    });
+if (result === "denied") {
+  console.log("Fitur notifikasi tidak diijinkan.");
+  return;
+} else if (result === "default") {
+  console.error("Pengguna menutup kotak dialog permintaan ijin.");
+  return;
+}
+
+if (('PushManager' in window)) {
+  navigator.serviceWorker.getRegistration().then(function(registration) {
+      registration.pushManager.subscribe({
+          userVisibleOnly: true,
+          //generate this from google firebase and get the id sender -> generate vapid key from terminal and get the public key and then paste the key in the bottom
+          applicationServerKey: urlBase64ToUint8Array("BHVSp_vS5A_4p39B9C7BpSqkeeNM-tdgaWYyV1N63BZg2wimYfd_cIFZAOKbybhA_mZU_UPQF2I5Un2ZcZVF6kA")
+      }).then(function(subscribe) {
+          console.log('Berhasil melakukan subscribe dengan endpoint: ', subscribe.endpoint);
+          console.log('Berhasil melakukan subscribe dengan p256dh key: ', btoa(String.fromCharCode.apply(
+              null, new Uint8Array(subscribe.getKey('p256dh')))));
+          console.log('Berhasil melakukan subscribe dengan auth key: ', btoa(String.fromCharCode.apply(
+              null, new Uint8Array(subscribe.getKey('auth')))));
+      }).catch(function(e) {
+          console.error('Tidak dapat melakukan subscribe ', e.message);
+      });
+  });
 }
 });
 }
-  }
+}
 
-  function urlBase64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding)
-        .replace(/-/g, '+')
-        .replace(/_/g, '/');
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
-    for (let i = 0; i < rawData.length; ++i) {
-        outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+      .replace(/-/g, '+')
+      .replace(/_/g, '/');
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
 }
 
