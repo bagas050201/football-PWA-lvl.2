bf08f1b6180b38deb620e7684578e1c224fbedb8
Muhammad Bagas Pradana <54340164+bagas050201@users.noreply.github.com>
2020-11-02 15:04:57 +0700

    update version 4.3

--- service-worker.js
+++ service-worker.js
@@ -1,102 +1,4 @@
-const CACHE_NAME = "bola spanyol-v4.0";
-var urlsToCache = [
-  "/",
-  "/nav.html",
-  "/index.html",
-  "/manifest.json",
-  "/push.js",
-  "detail_info.html",
-  "/pages/klasemen.html",
-  "/pages/favorite.html",
-  "/pages/information.html",
-  "/css/materialize.min.css",
-  "/css/materialize.css",
-  "/js/materialize.min.js",
-  "/js/materialize.js",
-  "/js/api.js",
-  "/js/idb.js",
-  "/js/db.js",
-  "/js/nav.js",
-  "/js/register_sw.js",
-  "/icon/icon-48new.png",
-  "/icon/icon-96new.png",
-  "/icon/icon-192new.png",
-  "/icon/icon-512new.png",
-  "/icon/maskable_icon.png",
-  "https://fonts.googleapis.com/icon?family=Material+Icons",
-  "https://api.football-data.org/v2/competitions/2014/standings",
-  "https://api.football-data.org/v2/competitions/2014/teams",
-  "https://api.football-data.org/v2/teams"
-];
- 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-  self.addEventListener("fetch", function(event) {
-    const base_url = "https://api.football-data.org/v2/";
-    if (event.request.url.indexOf(base_url) > -1) {
-        event.respondWith(
-            caches.open(CACHE_NAME).then(function(cache) {
-                return fetch(event.request).then(function(response) {
-                    cache.put(event.request.url, response.clone());
-                    return response;
-                })
-            })
-        );
-    } else {
-        event.respondWith(
-            caches.match(event.request, {'ignoreSearch': true}).then(function(response) {
-                return response || fetch (event.request);
-            })
-        )
-    }
-});
-
-
-  self.addEventListener("activate", event => {
-    event.waitUntil(
-      caches.keys().then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            if (cacheName != CACHE_NAME) {
-              console.log("ServiceWorker: cache " + cacheName + " dihapus");
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-    );
-  });
-
-//new line push event API
-self.addEventListener('push', function(event) {
-  var body;
-  if (event.data) {
-    body = event.data.text();
-  } else {
-    body = 'Push message no payload';
-  }
-  var options = {
-    body: body,
-    icon: 'icon/icon-192new.png',
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: 1
-    }
-  };
-  event.waitUntil(
-    self.registration.showNotification('Push Notification', options)
-  );
-});
-
-
-/**const CACHE_NAME = "bola spanyol-v3.8";
+const CACHE_NAME = "bola spanyol-v4.3";
 var urlsToCache = [
   "/",
   "/nav.html",
@@ -122,7 +24,14 @@ var urlsToCache = [
   "/icon/icon-512new.png",
   "/icon/maskable_icon.png"
 ];
- 
+
+/*
+"https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://api.football-data.org/v2/competitions/2014/standings",
+  "https://api.football-data.org/v2/competitions/2014/teams",
+  "https://api.football-data.org/v2/teams"
+  */
+
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -189,4 +98,3 @@ self.addEventListener('push', function(event) {
   );
 });
 
-*/
