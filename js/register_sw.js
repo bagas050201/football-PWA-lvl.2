e1a5715110f327ab450d2f2bea231899405f99b1
Muhammad Bagas Pradana <54340164+bagas050201@users.noreply.github.com>
2020-11-03 16:38:46 +0700

    beautify

--- js/register_sw.js
+++ js/register_sw.js
@@ -42,15 +42,8 @@ if (('PushManager' in window)) {
               null, new Uint8Array(subscribe.getKey('p256dh')))));
             console.log('Berhasil melakukan subscribe dengan auth key: ', btoa(String.fromCharCode.apply(
               null, new Uint8Array(subscribe.getKey('auth')))));
-      })/*
-      .then(function(subscribe) {
-          console.log('Berhasil melakukan subscribe dengan endpoint: ', subscribe.endpoint);
-          console.log('Berhasil melakukan subscribe dengan p256dh key: ', btoa(String.fromCharCode.apply(
-              null, new Uint8Array(subscribe.getKey('p256dh')))));
-          console.log('Berhasil melakukan subscribe dengan auth key: ', btoa(String.fromCharCode.apply(
-              null, new Uint8Array(subscribe.getKey('auth')))));
       })
-      */.catch(function(e) {
+      .catch(function(e) {
           console.error('Tidak dapat melakukan subscribe ', e.message);
       });
 
